3852a8d4d0f6515a01a4b844d0e7a4d284614a13
recoilme <vadim-kulibaba@yandex.ru>
2020-01-30 12:22:53 +0300

    some stupid tests

--- core/persist/log_test.go
+++ core/persist/log_test.go
@@ -1,6 +1,9 @@
 package persist
 
 import (
+	"fmt"
+	"strconv"
+	"sync"
 	"testing"
 )
 
@@ -15,15 +18,37 @@ func TestAppendCrcHex(t *testing.T) {
 }
 
 func TestParseFileName(t *testing.T) {
-	if res := getTableFromFileName(getTableFileName("video_log_buffer (...)")); res != "video_log_buffer" {
+	if res := getTableFromFileName(getTableFileName("video_log_buffer (...)", false)); res != "video_log_buffer" {
 		t.Errorf("Could not parse filename: got '%s' instead of 'video_log_buffer'", res)
 	}
 
-	if res := getTableFromFileName(getTableFileName("video_log(...)")); res != "video_log" {
+	if res := getTableFromFileName(getTableFileName("video_log(...)", false)); res != "video_log" {
 		t.Errorf("Could not parse filename: got '%s' instead of 'video_log'", res)
 	}
 
 	if res := getTableFromFileName("random_stuff"); res != "" {
 		t.Errorf("Could not parse filename: got '%s' instead of ''", res)
 	}
+}
+
+func TestBuffer(t *testing.T) {
+	var wg sync.WaitGroup
+	file := "table1"
+	const len = 4
+
+	append := func(i int) {
+		defer wg.Done()
+		k := ("Key:" + strconv.Itoa(i))
+		err := Write(fmt.Sprintf("%s.%d", file, i%2), []byte(k), true)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	for i := 1; i <= len; i++ {
+		wg.Add(1)
+		go append(i)
+
+	}
+	wg.Wait()
+	//fmt.Println(string(tableBufMap.v["table1.0"].values.b.Bytes()))
 }
